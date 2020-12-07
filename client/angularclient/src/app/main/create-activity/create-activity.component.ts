118de740ffde0e8fad697021c69b18ba8817e116
Cornelia Bea Persson <corneliap@live.se>
2020-12-07 16:03:33 +0100

    byggt ut Planera aktivitet

--- client/angularclient/src/app/main/create-activity/create-activity.component.ts
+++ client/angularclient/src/app/main/create-activity/create-activity.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-create-activity',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateActivityComponent implements OnInit {
 
-  constructor() { }
+  showPlanActivity = false;
+  showCurrentActivities = false;
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
-
 }
