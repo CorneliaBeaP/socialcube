a22e130c61b3e57f2462fc072fd93510ed54241b
Cornelia Bea Persson <corneliap@live.se>
2020-12-14 17:45:48 +0100

    Lagt in paths för profile och admin

--- client/angularclient/src/app/app-routing.module.ts
+++ client/angularclient/src/app/app-routing.module.ts
@@ -4,11 +4,15 @@ import { UserListComponent} from "./userlist/user-list/user-list.component";
 import {LoginComponent} from "./login/login.component";
 import {MainComponent} from "./main/main.component";
 import {AuthGuard} from "./auth/auth.guard";
+import {ProfileComponent} from "./profile/profile.component";
+import {AdministrationComponent} from "./administration/administration.component";
 
 const routes: Routes = [
   {path: 'users', component: UserListComponent},
   {path: 'login', component: LoginComponent},
   {path: 'home', component: MainComponent, canActivate: [AuthGuard]},
+  {path: 'profile', component: ProfileComponent, canActivate: [AuthGuard]},
+  {path: 'admin', component: AdministrationComponent, canActivate: [AuthGuard]},
   {path: '', redirectTo: '/login', pathMatch: 'full'}
 ];
 
--- client/angularclient/src/app/navigation/header/header.component.ts
+++ client/angularclient/src/app/navigation/header/header.component.ts
@@ -26,4 +26,8 @@ export class HeaderComponent implements OnInit {
       this.isAdmin = true;
     }
   }
+
+  goToProfile(){
+
+  }
 }
