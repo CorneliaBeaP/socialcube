18c9cbaec5c1de29d13272afd54426013edbdd43
Cornelia Bea Persson <corneliap@live.se>
2020-12-21 15:37:04 +0100

    Om man tackar nej till ett event göms det

--- client/angularclient/src/app/main/activity-cards/activity-cards.component.ts
+++ client/angularclient/src/app/main/activity-cards/activity-cards.component.ts
@@ -13,6 +13,7 @@ export class ActivityCardsComponent implements OnInit, OnDestroy {
 
   activities: Activity[];
   subscription: Subscription;
+  declinedActivityIDs: number[];
 
   constructor(private activityService: ActivityService, private loginService: LoginService) {
   }
@@ -22,7 +23,10 @@ export class ActivityCardsComponent implements OnInit, OnDestroy {
       this.activities = activityarray;
       this.sortByCreatedDate();
       this.activities = this.activities.reverse();
+      this.getDeclinedActivityIDs();
+      this.sortAwayDeclinedActivities();
     });
+
   }
 
   public sortByCreatedDate(): void {
@@ -37,13 +41,41 @@ export class ActivityCardsComponent implements OnInit, OnDestroy {
     this.activityService.attendActivity(this.loginService.getUserValue().id, activityid);
   }
 
-  declineEvent(event) {
-    //TODO: om man trycker på knappen ist för Xet så tas hela raddan bort
-    // event.target.parentNode.parentNode.parentNode.hidden = true;
-  //  TODO: fortsätt här så att elementet stay hidden när man uppdaterar sidan
+  declineEvent(id: number) {
+    let declinedAs: number[] = JSON.parse(localStorage.getItem('declinedActivityIDs'));
+    if (localStorage.getItem('declinedActivityIDs') == null) {
+      declinedAs = [id];
+    } else {
+      if (!declinedAs.includes(id)) {
+        declinedAs.push(id);
+      }
+    }
+    location.reload();
+    localStorage.setItem('declinedActivityIDs', JSON.stringify(declinedAs));
   }
 
   ngOnDestroy(): void {
     this.subscription.unsubscribe();
+  }
+
+  public getDeclinedActivityIDs() {
+    this.declinedActivityIDs = JSON.parse(localStorage.getItem('declinedActivityIDs'));
+    console.log(this.declinedActivityIDs);
+  }
+
+  sortAwayDeclinedActivities() {
+    console.log(this.activities);
+    if (!(this.declinedActivityIDs == null)) {
+      this.declinedActivityIDs.forEach(id => {
+        this.activities.forEach(activity => {
+          if (activity.id == id) {
+            let index = this.activities.indexOf(activity);
+            if (index > -1) {
+              this.activities.splice(index, 1);
+            }
+          }
+        });
+      });
+    }
   }
 }
