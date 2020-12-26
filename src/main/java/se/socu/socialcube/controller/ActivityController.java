77056ace0a78d9e60a39ecb4f3eaec56a3f81d49
Cornelia Bea Persson <corneliap@live.se>
2020-12-26 13:58:05 +0100

    rensat kommentarer i backend

--- src/main/java/se/socu/socialcube/controller/ActivityController.java
+++ src/main/java/se/socu/socialcube/controller/ActivityController.java
@@ -23,45 +23,14 @@ public class ActivityController {
     private final ActivityService activityService;
     private final UserService userService;
 
-//    @PostMapping(path = "/activity")
-//    public void addActivity(@RequestBody String activity) {
-//        System.out.println("Sparar aktivitet:" + activity);
-////        activityService.saveActivityDTO(activityDTO);
-//    }
-
-//    @PostMapping(path = "/home/add", consumes = MediaType.APPLICATION_JSON_VALUE)
-//    public String saveActivity(@RequestBody ActivityDTO activityDTO) {
-//        System.out.println("Mottagit UserDTO");
-//        activityService.saveActivityDTO(activityDTO);
-//        return "Ok!";
-//    }
-
     @GetMapping(path = "/api/home/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ArrayList<ActivityDTO> getAllActivitiesForCompany(@PathVariable long id) {
         System.out.println("Skickar aktiviteter...");
         return activityService.findAllActivitiesByCompany_organizationnumber(id);
     }
 
-//    @PostMapping(value = "/api/activity/add", produces = MediaType.TEXT_PLAIN_VALUE)
-//    public ResponseEntity<String> save(@RequestBody ActivityDTO activityDTO) {
-//        HttpHeaders httpHeaders = new HttpHeaders();
-//        httpHeaders.set("Access-Control-Allow-Origin", "*");
-//        System.out.println(ResponseEntity.ok().header("Access-Control-Allow-Origin", "*"));
-//        System.out.println("save ok");
-//        return ResponseEntity.ok().headers(httpHeaders).body("Ok!");
-//    }
-
     @PostMapping(path = "/api/activity/add", produces = MediaType.APPLICATION_JSON_VALUE)
     public Response save(@RequestBody ActivityDTO activityDTO) {
-//        System.out.println("Beskrivning: " + activityDTO.getDescriptionsocu());
-//        System.out.println("Typ: " + activityDTO.getActivitytype());
-//        System.out.println("Aktivitetsdatum: " + activityDTO.getActivitydate());
-//        System.out.println("Aktivitetsdatum: " + activityDTO.getActivitydate().toLocalTime());
-//        System.out.println("Organisationsnummer: " + activityDTO.getCompanyorganizationnumber());
-//        System.out.println("LocationAddress: " + activityDTO.getLocationaddress());
-//        System.out.println("LocationName: " + activityDTO.getLocationname());
-//        System.out.println("Skapad av id: " + activityDTO.getCreatedbyid());
-//        System.out.println("RSVP-date: " + activityDTO.getRsvpdate());
         System.out.println("Lägger till aktivitet...");
         activityService.saveActivityDTO(activityDTO);
         Response response = new Response();
@@ -69,13 +38,6 @@ public class ActivityController {
         response.setStatus("OK");
         return response;
     }
-
-//    @PostMapping(path = "/api/activity/add", produces = MediaType.APPLICATION_JSON_VALUE)
-//    public ResponseEntity<ActivityDTO> receiveString(@RequestBody ActivityDTO stringy) {
-//        System.out.println(stringy.toString());
-//        ActivityDTO activityDTO = new ActivityDTO();
-//        return ResponseEntity.ok().body(activityDTO);
-//    }
 
     @PostMapping(path = "/api/activity/attendactivity")
     public Response attendActivity(@RequestBody String[] info) {
