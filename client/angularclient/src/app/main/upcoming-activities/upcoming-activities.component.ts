56843460eacbabbf733122eb01ef0c81d64786e0
Cornelia Bea Persson <corneliap@live.se>
2020-12-26 18:18:44 +0100

    event som redan varit sorteras ut i kommande evenemang

--- client/angularclient/src/app/main/upcoming-activities/upcoming-activities.component.ts
+++ client/angularclient/src/app/main/upcoming-activities/upcoming-activities.component.ts
@@ -21,8 +21,30 @@ export class UpcomingActivitiesComponent implements OnInit {
 
   getActivities() {
     this.activityService.getattendedActivities(this.loginService.getUserValue().id).subscribe(data => {
-      this.activities = data;
+      this.activities =this.sortAwayExpiredActivities(data);
     });
+  }
+  sortAwayExpiredActivities(list: Activity[]) {
+    let today = new Date();
+    let itemsToRemove = [];
+    list.forEach((activity) => {
+      let activityDate = activity.activitydate;
+      if (activityDate[0] < today.getFullYear()) {
+        itemsToRemove.push(activity);
+      } else if (activityDate[0] == today.getFullYear()) {
+        if (activityDate[1] < (today.getMonth() + 1)) {
+          itemsToRemove.push(activity);
+        } else if (activityDate[1] == (today.getMonth() + 1)) {
+          if (activityDate[2] < today.getDate()) {
+            itemsToRemove.push(activity);
+          }
+        }
+      }
+    });
+    itemsToRemove.forEach((activity) => {
+      list.splice(list.indexOf(activity), 1);
+    });
+    return list;
   }
 
 }
