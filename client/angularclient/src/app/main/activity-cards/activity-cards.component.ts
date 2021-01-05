67c22b19f7c748b732921c6ec2ace5e38b2084e8
Cornelia Bea Persson <corneliap@live.se>
2021-01-05 14:15:59 +0100

    modal stängs när man redigerat en aktivitet

--- client/angularclient/src/app/main/activity-cards/activity-cards.component.ts
+++ client/angularclient/src/app/main/activity-cards/activity-cards.component.ts
@@ -171,8 +171,6 @@ export class ActivityCardsComponent implements OnInit, OnDestroy {
   openEditModal(activity: Activity){
     let modalRef = this.modalService.open(EditModalComponent);
     modalRef.componentInstance.activity = activity;
-    // modalRef['activity'] = activity;
-
   }
 
   ngOnDestroy(): void {
