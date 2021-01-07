37de9341ae721f92fbfdd4f5212cc36d450d6ccd
Cornelia Bea Persson <corneliap@live.se>
2021-01-07 12:34:57 +0100

    lagt till klasser JwtUtil och ReadPropertyFile samt 2 inceptors i angular

---
+++ src/main/java/se/socu/socialcube/jwt/JwtUtil.java
@@ -0,0 +1,62 @@
+package se.socu.socialcube.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import javax.crypto.spec.SecretKeySpec;
+import javax.xml.bind.DatatypeConverter;
+import java.io.IOException;
+import java.security.Key;
+import java.util.Date;
+
+public class JwtUtil {
+    private static String SECRET_KEY;
+
+    public JwtUtil() throws IOException {
+        ReadPropertyFile readPropertyFile = new ReadPropertyFile();
+        SECRET_KEY = readPropertyFile.getSecretKey();
+    }
+
+    //Sample method to construct a JWT
+    public String createJWT(String id, String issuer, String subject, long ttlMillis) {
+
+        //The JWT signature algorithm we will be using to sign the token
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        //We will sign our JWT with our ApiKey secret
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary(SECRET_KEY);
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+
+        //Let's set the JWT Claims
+        JwtBuilder builder = Jwts.builder().setId(id)
+                .setIssuedAt(now)
+                .setSubject(subject)
+                .setIssuer(issuer)
+                .signWith(signatureAlgorithm, signingKey);
+
+        //if it has been specified, let's add the expiration
+        if (ttlMillis >= 0) {
+            long expMillis = nowMillis + ttlMillis;
+            Date exp = new Date(expMillis);
+            builder.setExpiration(exp);
+        }
+
+        //Builds the JWT and serializes it to a compact, URL-safe string
+        return builder.compact();
+    }
+
+    public Claims decodeJWT(String jwt) {
+
+        //This line will throw an exception if it is not a signed JWS (as expected)
+        return Jwts.parser()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(SECRET_KEY))
+                .parseClaimsJws(jwt).getBody();
+    }
+
+
+}
---
+++ src/main/java/se/socu/socialcube/jwt/ReadPropertyFile.java
@@ -0,0 +1,14 @@
+package se.socu.socialcube.jwt;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadPropertyFile {
+    public String getSecretKey() throws IOException {
+        Properties properties = new Properties();
+        FileInputStream fileInputStream = new FileInputStream("src/main/resources/config.properties");
+        properties.load(fileInputStream);
+        return properties.getProperty("SECRET_KEY");
+    }
+}
