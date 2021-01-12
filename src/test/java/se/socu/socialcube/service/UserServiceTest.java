9c881f2e142f1f96f905894eebe6e31286b0074d
Cornelia Bea Persson <corneliap@live.se>
2021-01-12 16:30:39 +0100

    test ändra lösenord

--- src/test/java/se/socu/socialcube/service/UserServiceTest.java
+++ src/test/java/se/socu/socialcube/service/UserServiceTest.java
@@ -6,12 +6,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import se.socu.socialcube.DTO.UserDTO;
+import se.socu.socialcube.SocialcubeApplication;
 import se.socu.socialcube.entities.Company;
+import se.socu.socialcube.entities.Response;
 import se.socu.socialcube.entities.UserSocu;
 import se.socu.socialcube.entities.Usertype;
 import se.socu.socialcube.repository.CompanyRepository;
 import se.socu.socialcube.repository.UserRepository;
+import se.socu.socialcube.security.jwt.JwtUtil;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -21,6 +25,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -36,6 +42,8 @@ class UserServiceTest {
     @Autowired
     private CompanyRepository companyRepository;
 
+    BCryptPasswordEncoder passwordEncoder;
+
     Company company;
     Company company2;
     UserSocu socuuser;
@@ -46,16 +54,17 @@ class UserServiceTest {
 
     @BeforeEach
     void init() throws IOException {
+        passwordEncoder = new BCryptPasswordEncoder();
         company = new Company(5501010101L, "TestCompany AB");
-        socuuser = new UserSocu(Usertype.USER, "Anna Svensson", "anna.svensson@testcompany.com", "aOpTYjdls", "A007", "IT-avdelningen", company);
+        socuuser = new UserSocu(Usertype.USER, "Anna Svensson", "anna.svensson@testcompany.com", passwordEncoder.encode("aOpTYjdls"), "A007", "IT-avdelningen", company);
         socuuser.setId(1L);
-        socuuser2 = new UserSocu(Usertype.ADMIN, "Erik Eriksson", "erik.eriksson@testcompany.com", "p56GkdoUyyy", "A008", "IT-avdelningen", company);
+        socuuser2 = new UserSocu(Usertype.ADMIN, "Erik Eriksson", "erik.eriksson@testcompany.com", passwordEncoder.encode("p56GkdoUyyy"), "A008", "IT-avdelningen", company);
         socuuser2.setId(2L);
 
         company2 = new Company(5502020202L, "TestCompany2 AB");
-        socuuser3 = new UserSocu(Usertype.USER, "Angelina Björk", "angelina@testcompany2.com", "slkdjfSGslri", "", "Lager", company2);
+        socuuser3 = new UserSocu(Usertype.USER, "Angelina Björk", "angelina@testcompany2.com", passwordEncoder.encode("slkdjfSGslri"), "", "Lager", company2);
         socuuser3.setId(3L);
-        socuuser4 = new UserSocu(Usertype.ADMIN, "Hans Ek", "hans@testcompany2.com", "lsgjSJRGlgs1", "", "Service Desk", company2);
+        socuuser4 = new UserSocu(Usertype.ADMIN, "Hans Ek", "hans@testcompany2.com", passwordEncoder.encode("lsgjSJRGlgs1"), "", "Service Desk", company2);
         socuuser4.setId(4L);
 
         dto = new UserDTO();
@@ -71,6 +80,16 @@ class UserServiceTest {
         List<UserSocu> userSocuList = Arrays.asList(socuuser, socuuser2, socuuser3, socuuser4);
         companyRepository.saveAll(companyList);
         userRepository.saveAll(userSocuList);
+
+        Logger logger = Logger.getLogger(UserServiceTest.class.getName());
+        for (UserSocu u : userRepository.findAll()
+        ) {
+            String uid = u.getId() + "";
+            logger.log(Level.INFO, uid);
+            if(u.getId()>200){
+                logger.warning("ID överstiger 200");
+            }
+        }
     }
 
     @AfterEach
@@ -78,7 +97,7 @@ class UserServiceTest {
         userRepository.deleteAll();
         companyRepository.deleteAll();
         String folder = "client/angularclient/src/assets/ProfilePictures/";
-        for (int i = 1; i < 10; i++) {
+        for (int i = 1; i < 100; i++) {
             String fileName = i + ".png";
             Path path = Paths.get(folder + fileName);
             Files.deleteIfExists(path);
@@ -156,10 +175,10 @@ class UserServiceTest {
         assertEquals((IterableUtil.sizeOf(list) - 1), IterableUtil.sizeOf(userRepository.findAll()));
 
         userSocu = userRepository.findByEmail("anna.svensson@testcompany.com");
-        if (userSocu.isPresent()){
+        if (userSocu.isPresent()) {
             fail("Borttagning av användare misslyckades");
+        }
     }
-}
 
     @Test
     void saveImage() {
@@ -179,6 +198,12 @@ class UserServiceTest {
 
     @Test
     void changePassword() {
+        Optional<UserSocu> userSocu = userRepository.findByEmail("hans@testcompany2.com");
+        Response response = userService.changePassword("lsgjSJRGlgs1", "aAaAaAaA", userSocu.get().getId());
+        Optional<UserSocu> userSocu2 = userRepository.findByEmail("hans@testcompany2.com");
+        if (!(passwordEncoder.matches("aAaAaAaA", userSocu2.get().getPassword()))) {
+            fail("Ändring av lösenord misslyckades.");
+        }
     }
 
     @Test
