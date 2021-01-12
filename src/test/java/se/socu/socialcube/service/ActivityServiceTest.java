7544f28fe1d8589bfa68b4cd4cbabd4f3c3a045c
Cornelia Bea Persson <corneliap@live.se>
2021-01-12 22:19:31 +0100

    uppdaterat tester i ActivityServiceTest

--- src/test/java/se/socu/socialcube/service/ActivityServiceTest.java
+++ src/test/java/se/socu/socialcube/service/ActivityServiceTest.java
@@ -6,10 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import se.socu.socialcube.DTO.ActivityDTO;
-import se.socu.socialcube.entities.Activity;
-import se.socu.socialcube.entities.Company;
-import se.socu.socialcube.entities.UserSocu;
-import se.socu.socialcube.entities.Usertype;
+import se.socu.socialcube.entities.*;
 import se.socu.socialcube.repository.ActivityRepository;
 import se.socu.socialcube.repository.CompanyRepository;
 import se.socu.socialcube.repository.UserRepository;
@@ -24,6 +21,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -46,26 +45,23 @@ class ActivityServiceTest {
     @Autowired
     UserService userService;
 
-    private Activity activity1;
-    private Activity activity2;
-    private UserSocu userSocu1;
-    private UserSocu userSocu2;
     private ActivityDTO activityDTO;
-    private Long activity1id;
-
+    private List<Activity> activityList;
+    private List<UserSocu> userSocuList;
 
     @BeforeEach
     void setUp() {
+
         Company company = new Company(5501010101L, "TestCompany AB");
         LocalDateTime localDateTime = LocalDateTime.now();
 
-        userSocu1 = new UserSocu(Usertype.USER, "Anna Svensson", "anna.svensson@testcompany.com", "aOpTYjdls", "A007", "IT-avdelningen", company);
-        userSocu2 = new UserSocu(Usertype.ADMIN, "Erik Eriksson", "erik.eriksson@testcompany.com", "p56GkdoUyyy", "A008", "IT-avdelningen", company);
+        UserSocu userSocu1 = new UserSocu(Usertype.USER, "Anna Svensson", "anna.svensson@testcompany.com", "aOpTYjdls", "A007", "IT-avdelningen", company);
+        UserSocu userSocu2 = new UserSocu(Usertype.ADMIN, "Erik Eriksson", "erik.eriksson@testcompany.com", "p56GkdoUyyy", "A008", "IT-avdelningen", company);
         companyRepository.save(company);
         userRepository.save(userSocu1);
         userRepository.save(userSocu2);
 
-        activity1 = new Activity("Fika", localDateTime.plusDays(3), localDateTime.plusDays(2), localDateTime, "Fika för att fira av Fia", false, userSocu1, "Fikarummet", "Värtavägen 31, 11538 Stockholm", company);
+        Activity activity1 = new Activity("Fika", localDateTime.plusDays(3), localDateTime.plusDays(2), localDateTime, "Fika för att fira av Fia", false, userSocu1, "Fikarummet", "Värtavägen 31, 11538 Stockholm", company);
         repository.save(activity1);
 
         Optional<UserSocu> userSocuOptional = userRepository.findByEmail("anna.svensson@testcompany.com");
@@ -80,11 +76,23 @@ class ActivityServiceTest {
         activityDTO.setActivitytype("AW");
         activityDTO.setActivitydate(localDateTime.plusMonths(2));
         activityDTO.setRsvpdate(localDateTime.plusMonths(1));
-        Logger logger = Logger.getLogger(ActivityServiceTest.class.getName());
+
+        activityList = new ArrayList<>();
+        userSocuList = new ArrayList<>();
         for (Activity a : repository.findAll()
         ) {
-            logger.log(Level.INFO, a.getId() + "");
+            activityList.add(a);
         }
+        for (UserSocu u : userRepository.findAll()
+        ) {
+            userSocuList.add(u);
+        }
+
+//        Logger logger = Logger.getLogger(ActivityServiceTest.class.getName());
+//        for (Activity a : repository.findAll()
+//        ) {
+//            logger.log(Level.INFO, a.getId() + "");
+//        }
     }
 
     @AfterEach
@@ -98,6 +106,9 @@ class ActivityServiceTest {
             Path path = Paths.get(folder + fileName);
             Files.deleteIfExists(path);
         }
+
+        activityList.clear();
+        userSocuList.clear();
     }
 
     @Test
@@ -107,29 +118,114 @@ class ActivityServiceTest {
         activityService.saveActivityDTO(activityDTO);
         activities = repository.findAll();
         assertEquals(2, IterableUtil.sizeOf(activities));
-        Optional<Activity> activity = repository.findById(14L);
+        activityList.clear();
+        for (Activity a : activities
+        ) {
+            activityList.add(a);
+        }
+        Optional<Activity> activity = repository.findById(activityList.get(1).getId());
         assertEquals("AW", activity.get().getActivitytype());
         assertNotNull(activity);
     }
 
     @Test
     void declineActivity() {
+        Iterable<Activity> activities = repository.findAllDeclinedActivitiesByUsersocuId(userSocuList.get(0).getId());
+        assertEquals(0, IterableUtil.sizeOf(activities));
+        activityService.declineActivity(activityList.get(0).getId(), userSocuList.get(0).getId());
+        activities = repository.findAllDeclinedActivitiesByUsersocuId(userSocuList.get(0).getId());
+        assertEquals(1, IterableUtil.sizeOf(activities));
+        for (Activity a : activities
+        ) {
+            assertEquals(activityList.get(0).getId(), a.getId());
+        }
     }
 
     @Test
     void declineAttendedActivity() {
+        Iterable<Activity> attendedActivities = repository.findAllAttendedActivitiesByUsersocuId(userSocuList.get(0).getId());
+        Iterable<Activity> declinedActivitiesByUsersocuId = repository.findAllDeclinedActivitiesByUsersocuId(userSocuList.get(0).getId());
+        Iterable<UserSocu> attendees = userRepository.findAllAttendeesByActivityId(activityList.get(0).getId());
+        Iterable<UserSocu> decliners = userRepository.findAllDeclinersByActivityId(activityList.get(0).getId());
+
+        assertEquals(0, IterableUtil.sizeOf(attendedActivities));
+        assertEquals(0, IterableUtil.sizeOf(declinedActivitiesByUsersocuId));
+        assertEquals(0, IterableUtil.sizeOf(attendees));
+        assertEquals(0, IterableUtil.sizeOf(decliners));
+
+        activityService.attendActivity(userSocuList.get(0).getId(), activityList.get(0).getId());
+        attendedActivities = repository.findAllAttendedActivitiesByUsersocuId(userSocuList.get(0).getId());
+        declinedActivitiesByUsersocuId = repository.findAllDeclinedActivitiesByUsersocuId(userSocuList.get(0).getId());
+        attendees = userRepository.findAllAttendeesByActivityId(activityList.get(0).getId());
+        decliners = userRepository.findAllDeclinersByActivityId(activityList.get(0).getId());
+
+        assertEquals(1, IterableUtil.sizeOf(attendedActivities));
+        assertEquals(0, IterableUtil.sizeOf(declinedActivitiesByUsersocuId));
+        assertEquals(1, IterableUtil.sizeOf(attendees));
+        assertEquals(0, IterableUtil.sizeOf(decliners));
+
+        activityService.declineAttendedActivity(activityList.get(0).getId(), userSocuList.get(0).getId());
+        attendedActivities = repository.findAllAttendedActivitiesByUsersocuId(userSocuList.get(0).getId());
+        declinedActivitiesByUsersocuId = repository.findAllDeclinedActivitiesByUsersocuId(userSocuList.get(0).getId());
+        attendees = userRepository.findAllAttendeesByActivityId(activityList.get(0).getId());
+        decliners = userRepository.findAllDeclinersByActivityId(activityList.get(0).getId());
+
+        assertEquals(0, IterableUtil.sizeOf(attendedActivities));
+        assertEquals(1, IterableUtil.sizeOf(declinedActivitiesByUsersocuId));
+        assertEquals(0, IterableUtil.sizeOf(attendees));
+        assertEquals(1, IterableUtil.sizeOf(decliners));
     }
 
     @Test
     void attendDeclinedActivity() {
+        Iterable<Activity> attendedActivities = repository.findAllAttendedActivitiesByUsersocuId(userSocuList.get(0).getId());
+        Iterable<Activity> declinedActivitiesByUsersocuId = repository.findAllDeclinedActivitiesByUsersocuId(userSocuList.get(0).getId());
+        Iterable<UserSocu> attendees = userRepository.findAllAttendeesByActivityId(activityList.get(0).getId());
+        Iterable<UserSocu> decliners = userRepository.findAllDeclinersByActivityId(activityList.get(0).getId());
+
+        assertEquals(0, IterableUtil.sizeOf(attendedActivities));
+        assertEquals(0, IterableUtil.sizeOf(declinedActivitiesByUsersocuId));
+        assertEquals(0, IterableUtil.sizeOf(attendees));
+        assertEquals(0, IterableUtil.sizeOf(decliners));
+
+        activityService.declineActivity(activityList.get(0).getId(), userSocuList.get(0).getId());
+        attendedActivities = repository.findAllAttendedActivitiesByUsersocuId(userSocuList.get(0).getId());
+        declinedActivitiesByUsersocuId = repository.findAllDeclinedActivitiesByUsersocuId(userSocuList.get(0).getId());
+        attendees = userRepository.findAllAttendeesByActivityId(activityList.get(0).getId());
+        decliners = userRepository.findAllDeclinersByActivityId(activityList.get(0).getId());
+
+        assertEquals(0, IterableUtil.sizeOf(attendedActivities));
+        assertEquals(1, IterableUtil.sizeOf(declinedActivitiesByUsersocuId));
+        assertEquals(0, IterableUtil.sizeOf(attendees));
+        assertEquals(1, IterableUtil.sizeOf(decliners));
+
+        activityService.attendDeclinedActivity(userSocuList.get(0).getId(), activityList.get(0).getId());
+        attendedActivities = repository.findAllAttendedActivitiesByUsersocuId(userSocuList.get(0).getId());
+        declinedActivitiesByUsersocuId = repository.findAllDeclinedActivitiesByUsersocuId(userSocuList.get(0).getId());
+        attendees = userRepository.findAllAttendeesByActivityId(activityList.get(0).getId());
+        decliners = userRepository.findAllDeclinersByActivityId(activityList.get(0).getId());
+
+        assertEquals(1, IterableUtil.sizeOf(attendedActivities));
+        assertEquals(0, IterableUtil.sizeOf(declinedActivitiesByUsersocuId));
+        assertEquals(1, IterableUtil.sizeOf(attendees));
+        assertEquals(0, IterableUtil.sizeOf(decliners));
     }
 
     @Test
     void findAllActivitiesByCompany_organizationnumber() {
+        Iterable<ActivityDTO> activities = activityService.findAllActivitiesByCompany_organizationnumber(5501010101L);
+        assertEquals(1, IterableUtil.sizeOf(activities));
+        activities = activityService.findAllActivitiesByCompany_organizationnumber(909090);
+        assertEquals(0, IterableUtil.sizeOf(activities));
+        activities = activityService.findAllActivitiesByCompany_organizationnumber(5501670287L);
+        assertEquals(0, IterableUtil.sizeOf(activities));
     }
 
     @Test
     void attendActivity() {
+        activityService.attendActivity(userSocuList.get(0).getId(), activityList.get(0).getId());
+        Iterable<Activity> activities = repository.findAllAttendedActivitiesByUsersocuId(userSocuList.get(0).getId());
+        assertEquals(1, IterableUtil.sizeOf(activities));
     }
 
     @Test
