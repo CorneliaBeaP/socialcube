e951dfba653a73f736cdd8282725bc987737930f
Cornelia Bea Persson <corneliap@live.se>
2021-01-13 11:33:11 +0100

    tester för ReadPropertyFileTest

---
+++ src/test/java/se/socu/socialcube/security/jwt/ReadPropertyFileTest.java
@@ -0,0 +1,20 @@
+package se.socu.socialcube.security.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class ReadPropertyFileTest {
+
+    @Test
+    void getSecretKey() throws IOException {
+        ReadPropertyFile readPropertyFile = new ReadPropertyFile();
+       String secretkey = readPropertyFile.getSecretKey();
+       assertNotNull(secretkey);
+       assertEquals(31, secretkey.length());
+    }
+}
