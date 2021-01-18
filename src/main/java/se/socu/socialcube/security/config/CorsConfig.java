14d086924b82ca94d34446d31ab1fca1376f1edd
Cornelia Bea Persson <corneliap@live.se>
2021-01-18 14:30:46 +0100

    JavaDoc för CorsConfig

--- src/main/java/se/socu/socialcube/security/config/CorsConfig.java
+++ src/main/java/se/socu/socialcube/security/config/CorsConfig.java
@@ -6,6 +6,9 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * Configures Cross Origin Resource Sharing to allow all origins
+ */
 @Configuration
 @EnableWebMvc
 public class CorsConfig {
