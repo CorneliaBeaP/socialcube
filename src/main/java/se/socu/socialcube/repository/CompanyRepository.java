7ef5ede8f31fa3ee006f935f7052bb619ee29528
Cornelia Bea Persson <corneliap@live.se>
2021-01-18 14:41:34 +0100

    JavaDoc för CompanyRepository

--- src/main/java/se/socu/socialcube/repository/CompanyRepository.java
+++ src/main/java/se/socu/socialcube/repository/CompanyRepository.java
@@ -6,8 +6,17 @@ import se.socu.socialcube.entities.Company;
 
 import java.util.Optional;
 
+/**
+ * Repository that communicates with the database regarding things about the entity Company
+ */
 @Repository
 public interface CompanyRepository extends CrudRepository<Company, Long> {
+
+    /**
+     * Provides a specific Company
+     * @param id the organization number of the company
+     * @return the company (if there is one with that organization number)
+     */
     @Override
-    Optional<Company> findById(Long aLong);
+    Optional<Company> findById(Long id);
 }
