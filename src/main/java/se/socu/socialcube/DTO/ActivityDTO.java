4268af995b14b11fa5913c30c39c8f947afd9e38
Cornelia Bea Persson <corneliap@live.se>
2021-01-18 15:51:17 +0100

    JavaDoc för ActivityDTO och UserDTO

--- src/main/java/se/socu/socialcube/DTO/ActivityDTO.java
+++ src/main/java/se/socu/socialcube/DTO/ActivityDTO.java
@@ -5,23 +5,77 @@ import lombok.Setter;
 
 import java.time.LocalDateTime;
 
+/**
+ * Data Transfer Object for sending information about an activity to and from the client
+ */
 @Getter
 @Setter
 public class ActivityDTO {
 
+    /**
+     * Id from database of the activity
+     */
     private long id;
+
+    /**
+     * Type of the activity, ex After Work, Christmas party etc...
+     */
     private String activitytype;
+
+    /**
+     * The date of which the activity takes place on
+     */
     private LocalDateTime activitydate;
+
+    /**
+     * Eventual RSVP date, not obligatory
+     */
     private LocalDateTime rsvpdate;
+
+    /**
+     * The date of which the activity was created
+     */
     private LocalDateTime createddate;
+
+    /**
+     * A description of the activity
+     */
     private String descriptionsocu;
+
+    /**
+     * ID of the user that created the activity
+     */
     private long createdbyid;
+
+    /**
+     * The name of the place of which the activity takes place at
+     */
     private String locationname;
+
+    /**
+     * The address of the place of which the activity takes place at
+     */
     private String locationaddress;
+
+    /**
+     * The company the activity is registered to and the user who created the activity works at
+     */
     private long companyorganizationnumber;
+
+    /**
+     * The user that has created the activity
+     */
     private UserDTO createdBy;
+
+    /**
+     * Boolean describing if the activity is cancelled or not cancelled
+     */
     private boolean cancelled;
 
+    /**
+     * ToString-method providing information about the activityDTO
+     * @return a string containing information about id, activity type, activitydate, RSVP-date, date the activity is created, description of the activity, if the activity is cancelled or not and location name and adress where the activity takes place.
+     */
     @Override
     public String toString() {
         return "ActivityDTO{" +
--- src/main/java/se/socu/socialcube/DTO/UserDTO.java
+++ src/main/java/se/socu/socialcube/DTO/UserDTO.java
@@ -4,20 +4,62 @@ import lombok.Getter;
 import lombok.Setter;
 import se.socu.socialcube.entities.Usertype;
 
+/**
+ * Data Transfer Object for sending information about an user to and from the client
+ */
 @Getter
 @Setter
 public class UserDTO {
+
+    /**
+     * The users ID from the database
+     */
     private long id;
+
+    /**
+     * The usertype used to determine the users privileges
+     */
     private Usertype usertype;
+
+    /**
+     * The first and last name of the user
+     */
     private String name;
+
+    /**
+     * The email of the user
+     */
     private String email;
+
+    /**
+     * Eventual employment number of the user, not obligatory
+     */
     private String employmentnumber;
+
+    /**
+     * Eventual department the user works at, not obligatory
+     */
     private String department;
+
+    /**
+     * The organization number of the company the user works at
+     */
     private long companyorganizationnumber;
+
+    /**
+     * Jason Web Token that is provided after an user logs in
+     */
     private String token;
+
+    /**
+     * The name of the company the user works at
+     */
     private String companyname;
 
-
+    /**
+     * ToString-method that prints id, usertype, name, email, emloyment number, department, the organization number of the company the user works at and the users last created JWT.
+     * @return the string with information
+     */
     @Override
     public String toString() {
         return "UserDTO{" +
