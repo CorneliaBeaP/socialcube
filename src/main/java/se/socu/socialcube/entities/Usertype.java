536e219a5a89c8726250690549ac38e3bce778bc
Cornelia Bea Persson <corneliap@live.se>
2021-01-18 14:49:32 +0100

    JavaDoc för Usertype

--- src/main/java/se/socu/socialcube/entities/Usertype.java
+++ src/main/java/se/socu/socialcube/entities/Usertype.java
@@ -1,5 +1,8 @@
 package se.socu.socialcube.entities;
 
+/**
+ * Enumeration describing the usertype which is used to determine the users privileges.
+ */
 public enum Usertype {
     USER,
     ADMIN;
