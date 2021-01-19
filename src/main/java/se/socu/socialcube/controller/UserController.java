9bd865b323f540b595bf3c6b688106835d6cbdff
Cornelia Bea Persson <corneliap@live.se>
2021-01-19 16:03:10 +0100

    JavaDoc för UserController

--- src/main/java/se/socu/socialcube/controller/UserController.java
+++ src/main/java/se/socu/socialcube/controller/UserController.java
@@ -19,66 +19,101 @@ public class UserController {
         this.userService = userService;
     }
 
+    /**
+     * A service used to communicate with the repository which in turn communicates with the database
+     */
     private final UserService userService;
 
+    /**
+     * Provides all the users registered to a specific company
+     * @param id the companys id/organization number
+     * @return a list of all users registered to the company, as UserDTOs
+     */
     @GetMapping("/api/users/{id}")
     public List<UserDTO> getUsersForCompany(@PathVariable Long id) {
         return userService.getAllUserDTOsForCompany(id);
     }
 
+    /**
+     * Gets information about a new user by the client and sends it to the UserService to save in the database
+     * @param userDTO an UserDTO with the information about the new user
+     * @return a response with information about if the user was saved or not
+     */
     @PostMapping(path = "/api/users/add", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     public Response saveNewUser(@RequestBody UserDTO userDTO) {
         return userService.saveNewUser(userDTO);
     }
 
+    /**
+     * Gets information from the client about an user that is to be deleted and forward the information to the UserService
+     * @param id the id of the user
+     * @return a response about if the action to delete the user was successful or not
+     */
     @DeleteMapping("/api/users/delete/{id}")
     public Response deleteUser(@PathVariable Long id) {
-        System.out.println("Användare med id " + id + " är borttagen.");
         return userService.deleteUser(id);
     }
 
+    /**
+     * Gets a multipartfile from the client that a user has set as their profile picture and forward it to the UserService where the image is saved to the database
+     * @param multipartFile the file with the profile picture image
+     * @param id the id of the user
+     * @return a response with information about if the image was saved successfully or not
+     */
     @PostMapping(value = "/api/users/add/image/{id}")
-    public Response addProfilePicture(@RequestParam("name") MultipartFile multipartFile, @PathVariable Long id) throws Exception {
+    public Response addProfilePicture(@RequestParam("name") MultipartFile multipartFile, @PathVariable Long id){
         return userService.saveImage(multipartFile, id);
     }
 
+    /**
+     * Get information from the client about that a user wants to remove their profile picture and forwards that information to the UserService
+     * @param id the id of the user
+     * @return a response with information about if the image was removed successfully or not
+     */
     @GetMapping("/api/users/delete/image/{id}")
     public Response deleteProfilePicture(@PathVariable Long id) {
         return userService.deleteProfilePicture(id, false);
     }
 
+    /**
+     * Gets information from the client that a user wants to change their password and forwards it to the UserService
+     * @param token the JWT that is connected to the user that is sending the request
+     * @param passwordinfo an array that first contains the users old password, then their new password
+     * @return a response with information about if the password was updated or if there were errors
+     */
     @PutMapping(value = "/api/users/password/{token}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public Response changePassword(@PathVariable String token, @RequestBody String[] passwordinfo) {
         return userService.changePassword(passwordinfo[0], passwordinfo[1], userService.getUserIDFromJWT(token));
     }
 
+    /**
+     * Gets information from the client about a user having new user information, such as name, email and/or department and forwards it to the UserService.
+     * @param token the JWT that is connected to the user that is sending the request
+     * @param userinfo the information about the users name, email and department regardless if all or just one or two has been changed
+     * @return a response with information about if the user information was updated in the database successfully or if there were errors preventing this
+     */
     @PutMapping(value = "/api/users/update/{token}")
     public Response updateUserInformation(@PathVariable String token, @RequestBody String[] userinfo) {
         return userService.updateUserInformation(userService.getUserIDFromJWT(token), userinfo[0], userinfo[1], userinfo[2]);
     }
 
+    /**
+     * Gets information from the client when an user is trying to login and forwards it to the UserService.
+     * @param usercredentials an array that contains username and password in that order
+     * @return an empty UserDTO if the username/password is incorrect, or the UserDTO for the logged in user if the username/password is correct which also contains a Jason Web Token used for future communication between client and backend
+     */
     @PostMapping(path = "/api/login", produces = MediaType.APPLICATION_JSON_VALUE)
-    public UserDTO verifyCredentialsAndSendToken(@RequestBody String[] usercredentials) throws IOException {
-        System.out.println(LocalDateTime.now() + " Inlogg: " + userService.checkIfLoginCredentialsAreCorrectAndGetUser(usercredentials[0], usercredentials[1]).getId());
+    public UserDTO verifyCredentialsAndSendToken(@RequestBody String[] usercredentials){
         return userService.checkIfLoginCredentialsAreCorrectAndGetUser(usercredentials[0], usercredentials[1]);
     }
 
+    /**
+     * Gets a JWT from the client that is forwarded to the UserService to be decoded.
+     * @param token the JWT that is connected to the user that is sending the request
+     * @return an UserDTO with information about the user who is logged in and who sent the request
+     */
     @GetMapping(value = "/api/user/{token}")
-    public UserDTO getUserDTOFromJWT(@PathVariable String token) throws IOException {
-        System.out.println(LocalDateTime.now() + " Info om användare " + userService.getUserFromJWT(token).getId() + " har skickats");
+    public UserDTO getUserDTOFromJWT(@PathVariable String token){
         return userService.getUserFromJWT(token);
-    }
-
-    @GetMapping(value = "/api/getuserid/{token}")
-    public Response getUserIDfromJWT(@PathVariable String token) {
-        Response response = new Response();
-        long id = userService.getUserIDFromJWT(token);
-        response.setMessage(Long.toString(id));
-        if (id > 0) {
-            response.setStatus("OK");
-        } else {
-            response.setStatus("ERROR");
-        }
-        return response;
     }
 }
